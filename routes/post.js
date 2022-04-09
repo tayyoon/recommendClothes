877f4666f99dd1814082a92f54a31fc85bd8c91d
gofl26 <gofl26@naver.com>
2022-04-09 16:42:07 +0900

    4/9 16시40분 커밋
    postId와 commentId를 _id로 나타냄

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,68 @@
 const express = require('express');
 const Posts = require('../schemas/post');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const authMiddleware = require('../middlewares/auth-middleware');
 
 router.get('/', (req, res) => {
     res.send('this is root page');
+});
+
+//글 등록하기API
+router.post('/postWrite', async (req, res) => {
+    const today = new Date();
+    const date = today.toLocaleString();
+    const { userName, title, content, image, userId } = req.body;
+    
+    const createdPost = await Posts.create({
+        userId,
+        content,
+        title,
+        image,
+        userName,
+        date,
+    });
+    res.json({ post: createdPost });
+});
+
+//글 수정하기API
+router.put('/detail/:id', async (req, res) => {
+    const { id } = req.params;
+    const { content, title, image } = req.body;
+    const today = new Date();
+    const date = today.toLocaleString();
+
+    const createdPost = await Posts.updateOne(
+        { postId: Number(id) },
+        { $set: { content, title, image, date } }
+    );
+
+    res.json({ post: createdPost });
+});
+
+//글 상세조회API
+router.get('/detail/:id', async (req, res) => {
+    const { id } = req.params;
+    const o_id = new Object(id)
+    const [detail] = await Posts.find({ _id : o_id });
+    
+    res.json({
+        detail,
+    });
+});
+
+//글 삭제하기API
+router.delete('/detail/:id', async (req, res) => {
+    const { id } = req.params;
+    const o_id = new Object(id)
+    const existsPosts = await Posts.find({ _id : o_id });
+    if (existsPosts.length) {
+        await Posts.deleteOne({ _id : o_id });
+    }
+    const existsComments = await Comments.find({ postId: o_id });
+    if (existsComments.length) {
+        await Comments.deleteMany({ postId: o_id });
+    }
+
+    res.json({ success: true });
 });
 
 module.exports = router;
